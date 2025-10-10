df88d9d4b2f4bb7c36a379e2bda3d75e4f59627b
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-10-10 14:25:37 -0300

    Adicionado o teste para se caso for inserido credenciais erradas

--- cypress/e2e/loginuser/loginuser.cy.js
+++ cypress/e2e/loginuser/loginuser.cy.js
@@ -1,9 +1,10 @@
 describe('Login user', ()=> {
     beforeEach(() => {
         cy.visit('https://automationexercise.com/')
-        cy.createuser()
+        
     })
     it('Login User with correct email and password', () =>  {
+        cy.createuser()
         cy.visit('https://automationexercise.com/')
         cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
         cy.contains('h2','Login to your account')
@@ -13,5 +14,17 @@ describe('Login user', ()=> {
         cy.contains('a','Logout')
         cy.get('.shop-menu > .nav > :nth-child(5) > a').click()
         cy.contains('b', 'Account Deleted!')
+    })
+    it.only('Login User with incorrect email and password', () => {
+        cy.contains('a', 'Signup / Login')
+        cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
+        cy.contains('h2','Login to your account')
+        cy.get('[data-qa="login-email"]').type('teste4@outlook.com') //Precisa ter um usuário criado para testar
+        cy.get('[data-qa="login-password"]').type('wrongPassword')
+        cy.get('[data-qa="login-button"]').click()
+        cy.contains('p','Your email or password is incorrect!')
+        
+
+
     })
 })
