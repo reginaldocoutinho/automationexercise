7a99efde5f62afe59ea5934096b05ce4fdea14ae
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-12-03 12:09:54 -0300

    Adicionado a configuração do repositório mockawesome para registro dos relátórios de teste

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: 'cypress/results/json', // Diretório onde os JSONs serão salvos
+      overwrite: false, // Importante: não sobrescrever o arquivo a cada spec
+      html: true, // Não gerar HTML ainda, faremos isso manualmente depois do merge
+      json: true, // Gerar arquivos JSON
+      embeddedScreenshots: true,
+      charts: true,
+    },
   },
 });
