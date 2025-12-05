7ae5e7672741ab9cbf24fdb5125ca525de5eb563
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-12-05 13:28:48 -0300

    Criado novo teste relacionado ao Place Order, neste caso antes de realizar o checkout. 50min para criação

--- cypress/e2e/registeruser/registeruser.cy.js
+++ cypress/e2e/registeruser/registeruser.cy.js
@@ -4,37 +4,9 @@ describe('register user', () => {
     cy.url().should('eq', 'https://automationexercise.com/')
   })
   it('Sign Up', (email = 'testname7@outlook.com', senha = 'teste') => {
-      cy.contains('a', 'Signup / Login').click()
-      cy.contains('h2','New User Signup!')
-      cy.get('[data-qa="signup-name"]').type('testname')
-      cy.get('[data-qa="signup-email"]').type(email)
-      cy.get('[data-qa="signup-button"]').click()
-      cy.contains('b','Enter Account Information')
-      cy.get('#id_gender1').click()
-      cy.get('[data-qa="password"]').type(senha)
-      cy.get('[data-qa="days"]').select('30')
-      cy.get('[data-qa="months"]').select('October')
-      cy.get('[data-qa="years"]').select('1998')
-      cy.get('#newsletter').click()
-      cy.get('#optin').click()
-      cy.get('[data-qa="first_name"]').type("First name")
-      cy.get('[data-qa="last_name"]').type("Last name")
-      cy.get('[data-qa="company"]').type("Company")
-      cy.get('[data-qa="address"]').type("street addres")
-      cy.get('[data-qa="country"]').select('Canada')
-      cy.get('[data-qa="state"]').type('Ontario')
-      cy.get('[data-qa="city"]').type('Toronto')
-      cy.get('[data-qa="zipcode"]').type('M5V 3L9')
-      cy.get('[data-qa="mobile_number"]').type('5516991509030')
-      cy.get('[data-qa="create-account"]').click()
-      cy.contains('b','Account Created!')
-      cy.get('[data-qa="continue-button"]').click()
-      cy.contains('a','Logged in as')
-      cy.contains('a','Delete Account').click()
-      cy.contains('b', 'Account Deleted!')
-      cy.get('[data-qa="continue-button"]').click()
+      cy.createUser();
     }),
-    it.only('Place Order: Register while Checkout', () => {
+    it('Place Order: Register while Checkout', () => {
       cy.get('.features_items > :nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn'). click();
       cy.get('.modal-footer > .btn').click();
       cy.contains('a','Cart').click();
@@ -55,5 +27,30 @@ describe('register user', () => {
       cy.contains('button','Pay and Confirm Order').click();
       cy.contains('p','Congratulations! Your order has been confirmed!').should('be.visible');
       cy.deleteAccount();
+    })
+    it.only('Place Order: Register before Checkout', () => {
+      cy.contains('a', 'Signup / Login').click();
+      cy.contains('h2','New User Signup!');
+      cy.createUser();
+      cy.get('@userName').then((retrievedName) => {
+      cy.contains('b', retrievedName).should('be.visible');
+      cy.get('.features_items > :nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click();
+      cy.contains('a','Cart').click();
+      cy.get('.active').should('be.visible');
+      cy.get('.col-sm-6 > .btn').click();
+      cy.get('#address_delivery').should('be.visible');
+      cy.get('#address_invoice').should('be.visible');
+      cy.get('.form-control').type('Teste');
+      cy.contains('a','Place Order').click();
+      cy.get('[data-qa="name-on-card"]').type('teste man');
+      cy.get('[data-qa="card-number"]').type('12344565566');
+      cy.get('[data-qa="cvc"]').type('123');
+      cy.get('[data-qa="expiry-month"]').type('10');
+      cy.get('[data-qa="expiry-year"]').type(2026);
+      cy.contains('button','Pay and Confirm Order').click();
+      cy.contains('p','Congratulations! Your order has been confirmed!').should('be.visible');
+      cy.deleteAccount();
+    
+       })
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -61,44 +61,45 @@ Cypress.Commands.add('signUser', (email = 'teste7@outlook.com', password = 'test
         
 })
 
-Cypress.Commands.add('createUser', (email = 'teste7@outlook.com', password = 'teste') => {
-      cy.get('[data-qa="signup-name"]').type('testname')
-      cy.get('[data-qa="signup-email"]').type(email)
+Cypress.Commands.add('createUser', (userName = 'testname', email = 'teste7@outlook.com', password = 'teste') => {
+      cy.get('[data-qa="signup-name"]').type('testname');
+      cy.wrap(userName).as('userName'); 
+      cy.get('[data-qa="signup-email"]').type(email);
       cy.get('[data-qa="signup-button"]').click()
       // Captura o corpo da página e entra em um contexto síncrono (dentro do .then)
-cy.get('body').then(($body) => {
-    // Usamos jQuery (.find) para checar se o elemento existe E está visível.
-    if ($body.find('.signup-form > form > p:visible').length > 0) {
-        // Se a condição for verdadeira, execute o primeiro bloco:
-        cy.log('Parágrafo do formulário de cadastro visível, chamando cy.signUser()');
-        cy.signUser();
-    } else {
-        // Se a condição for falsa (o elemento não está visível), execute o bloco else:
-        cy.log('Parágrafo não visível, prosseguindo com o preenchimento manual.');
-        cy.contains('b','Enter Account Information');
-        cy.get('#id_gender1').click();
-        // Certifique-se de que 'password' está definida em seu teste
-        cy.get('[data-qa="password"]').type(password);
-        cy.get('[data-qa="days"]').select('30');
-        cy.get('[data-qa="months"]').select('October');
-        cy.get('[data-qa="years"]').select('1998');
-        cy.get('#newsletter').click();
-        cy.get('#optin').click();
-        cy.get('[data-qa="first_name"]').type("First name");
-        cy.get('[data-qa="last_name"]').type("Last name");
-        cy.get('[data-qa="company"]').type("Company");
-        cy.get('[data-qa="address"]').type("street addres");
-        cy.get('[data-qa="country"]').select('Canada');
-        cy.get('[data-qa="state"]').type('Ontario');
-        cy.get('[data-qa="city"]').type('Toronto');
-        cy.get('[data-qa="zipcode"]').type('M5V 3L9');
-        cy.get('[data-qa="mobile_number"]').type('5516991509030');
-        cy.get('[data-qa="create-account"]').click();
-        
-        // Validações após a ação:
-        cy.contains('b','Account Created!');
-        cy.get('[data-qa="continue-button"]').click();
-    }
+      cy.get('body').then(($body) => {
+      // Usamos jQuery (.find) para checar se o elemento existe E está visível.
+      if ($body.find('.signup-form > form > p:visible').length > 0) {
+            // Se a condição for verdadeira, execute o primeiro bloco:
+            cy.log('Parágrafo do formulário de cadastro visível, chamando cy.signUser()');
+            cy.signUser();
+      } else {
+            // Se a condição for falsa (o elemento não está visível), execute o bloco else:
+            cy.log('Parágrafo não visível, prosseguindo com o preenchimento manual.');
+            cy.contains('b','Enter Account Information');
+            cy.get('#id_gender1').click();
+            // Certifique-se de que 'password' está definida em seu teste
+            cy.get('[data-qa="password"]').type(password);
+            cy.get('[data-qa="days"]').select('30');
+            cy.get('[data-qa="months"]').select('October');
+            cy.get('[data-qa="years"]').select('1998');
+            cy.get('#newsletter').click();
+            cy.get('#optin').click();
+            cy.get('[data-qa="first_name"]').type("First name");
+            cy.get('[data-qa="last_name"]').type("Last name");
+            cy.get('[data-qa="company"]').type("Company");
+            cy.get('[data-qa="address"]').type("street addres");
+            cy.get('[data-qa="country"]').select('Canada');
+            cy.get('[data-qa="state"]').type('Ontario');
+            cy.get('[data-qa="city"]').type('Toronto');
+            cy.get('[data-qa="zipcode"]').type('M5V 3L9');
+            cy.get('[data-qa="mobile_number"]').type('5516991509030');
+            cy.get('[data-qa="create-account"]').click();
+
+            // Validações após a ação:
+            cy.contains('b','Account Created!');
+            cy.get('[data-qa="continue-button"]').click();
+      }
 });
 
 
